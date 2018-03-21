c0d2ef9f83d5838b16d32fc462855b7c671f73b8
heyadong <502168573@qq.com>
2018-03-21 15:46:26 +0800

    flask 搭建微电影网站,day3 首页及首页轮播图和搜索页面

--- app/blog/views.py
+++ app/blog/views.py
@@ -17,14 +17,62 @@ def index():
     # user = Admin.query.filter_by(role_id=1).first()
     # print(request.args.get('name'))
     return render_template('index.html')
-
+# 登陆
 @blog.route('/login/')
 # 注意路由使用'/login/'
 def login():
     return render_template('home/login.html')
-
+# 登出
 @blog.route('/logout/')
 def logout():
     # redirect()重定向到登陆页面
     return redirect(url_for("blog.login"))
+
+
+@blog.route('/password/')
+# 更改密码
+def password():
+    return render_template('home/password.html')
+
+# 用户中心页面
+@blog.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+# 首页
+@blog.route('/index/')
+def index1():
+    return render_template('home/index.html')
+
+# 评论页面
+@blog.route('/comment/')
+def comment():
+    return render_template('home/comment.html')
+
+# 电影收藏
+@blog.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
+
+# 注册
+@blog.route('/register/')
+def register():
+    return render_template('home/register.html')
+
+# 登陆日志
+@blog.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+
+@blog.route('/animation/')
+# 首页轮播页面
+def animation():
+    return render_template("home/animation.html")
+
+# 搜索
+@blog.route('/search/')
+def search():
+    return render_template("home/search.html")
+
 
